77184f8401e4da63d244e51fedab507c8fcda754
bobby9123138458@gmail.com <bobby9123138458@gmail.com>
2023-05-15 22:38:20 +0530

    DOM MAniputlation using ID

---
+++ 2ndassign.js
@@ -0,0 +1,8 @@
+var title=document.getElementById('header-title')
+//title.textContent='NEW item Listener';
+title.innerText="Items Title";
+var header=document.getElementById('main-header')
+header.style.border="yellow solid 5px "
+let element=document.getElementById('element');
+element.style.fontWeight="Bold"
+element.style.color='green'
