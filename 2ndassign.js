a6e0c0a376e09b9e98d1bac5d0ada8dda262cced
bobby9123138458@gmail.com <bobby9123138458@gmail.com>
2023-05-16 09:36:24 +0530

    queryselector & all

--- 2ndassign.js
+++ 2ndassign.js
@@ -1,9 +1,10 @@
-const items=document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor='green';
-for(let i=0;i<items.length;i++){
-    items[i].style.backgroundColor='lightgrey';
+const li2=document.querySelector('.list-group-item:nth-child(2)');
+li2.style.backgroundColor='green';
+const li3=document.querySelector('.list-group-item:nth-child(3)');
+li3.style.display='none';
+const ui=document.querySelectorAll('.list-group-item');
+ui[1].style.color='green';
+const odd=document.querySelectorAll('li:nth-child(odd)');
+for (let i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
 }
-const li=document.getElementsByTagName('li');
-for(let i=0;i<li.length;i++){
-         li[i].style.fontWeight='bold';
-    }
