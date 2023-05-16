fbdb9bff35060fe7fc9e5fbe0581a5f0df42d861
bobby9123138458@gmail.com <bobby9123138458@gmail.com>
2023-05-16 19:42:52 +0530

    child&parent,push

--- 2ndassign.js
+++ 2ndassign.js
@@ -1,10 +1,18 @@
-const li2=document.querySelector('.list-group-item:nth-child(2)');
-li2.style.backgroundColor='green';
-const li3=document.querySelector('.list-group-item:nth-child(3)');
-li3.style.display='none';
-const ui=document.querySelectorAll('.list-group-item');
-ui[1].style.color='green';
-const odd=document.querySelectorAll('li:nth-child(odd)');
-for (let i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor='green';
-}
+var newDiv = document.createElement('div');
+newDiv.className='divclass';
+newDiv.id='divid';
+newDiv.setAttribute=('title','divtitle');
+var newDivtext = document.createTextNode('Hello');
+newDiv.appendChild(newDivtext);
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+container.insertBefore(newDiv,h1);
+// insertion in list
+var li =document.createElement('li');
+var litext=document.createTextNode('Hello');
+li.className='list-group';
+li.appendChild(litext);
+var list=document.getElementById('items');
+var list1=list.firstChild;
+console.log(li);
+list.insertBefore(li,list1);
