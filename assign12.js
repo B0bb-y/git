bc3b3d42576d407a8be9127319688c27a7439f3c
bobby9123138458@gmail.com <bobby9123138458@gmail.com>
2023-05-19 09:42:47 +0530

    assign 12 delete button created in created list

--- assign12.js
+++ assign12.js
@@ -15,6 +15,25 @@ document.getElementById('input').addEventListener('submit',function(e){
     document.getElementById('email').value='';
     updateUserList(existuser);
 });
+function deleteUser(email) {
+    var existuser = JSON.parse(localStorage.getItem('users')) || [];
+  
+    // Find the index of the user with the specified email
+    var index = existuser.findIndex(function(user) {
+      return user.email === email;
+    });
+  
+    if (index !== -1) {
+      // Remove the user from the existingUsers array
+      existuser.splice(index, 1);
+  
+      // Update the user data in local storage
+      localStorage.setItem('users', JSON.stringify(existuser));
+  
+      // Update the user list display
+      updateUserList(existuser);
+    }
+  }
 function updateUserList(users){
     var listElement=document.getElementById('list');
     listElement.innerHTML='';
@@ -23,7 +42,16 @@ function updateUserList(users){
         var listItem = document.createElement('li');
         listItem.textContent = user.name + '  '+ user.email;
         listElement.appendChild(listItem);
+        var deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.setAttribute('data-email', user.email);
+        deleteButton.addEventListener('click', function() {
+            var email = this.getAttribute('data-email');
+            deleteUser(email);
+        });
+        listItem.appendChild(deleteButton);
+        listElement.appendChild(listItem);
     }
 };
 var storedUsers = JSON.parse(localStorage.getItem('users')) || [];
-    updateUserList(storedUsers);
+updateUserList(storedUsers);
